91e06d82dda3285364bb70438d8b6498a28bb6b9
scevallos <sebastianlcevallos@gmail.com>
2021-10-04 19:49:28 -0400

    Complete go script to launch ABC

--- main.go
+++ main.go
@@ -1,40 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/picatz/roku"
 )
+
+const ABCAppId = "73376"
 
 type Config struct {
 	DeviceName string `envconfig:"DEVICE_NAME" required:"true"`
 }
 
 func main() {
-	fmt.Println("test")
-
 	conf := &Config{}
 	if err := envconfig.Process("", conf); err != nil {
 		log.Fatalf("invalid config: %s", err.Error())
 	}
 
-
 	devices, err := roku.Find(roku.DefaultWaitTime)
 	if err != nil {
-		panic(err)
+		log.Fatalf("SSDP request looking for Roku devices failed")
 	}
-
-	fmt.Println("len(devices)", len(devices))
 
 	for _, device := range devices {
 		if device != nil {
 			info, err := device.DeviceInfo()
 			if err == nil {
 				if info.FriendlyDeviceName == conf.DeviceName {
-					// launch ABC app
-					// keys needed to navigate to ABC Live
+					if err := device.LaunchApp(ABCAppId, nil); err != nil {
+						panic(err)
+					}
+
+					// app takes a long ass time to load
+					time.Sleep(10 * time.Second)
+
+					// keys needed to navigate to ABC Live TV
+					device.Keypress(roku.RightKey)
+					time.Sleep(2 * time.Second)
+					device.Keypress(roku.RightKey)
+					time.Sleep(2 * time.Second)
+					device.Keypress(roku.SelectKey)
+
+					// live TV takes an even longer time to load
+					time.Sleep(12 * time.Second)
+
+					// navigate to the "Watch" button and click it
+					device.Keypress(roku.DownKey)
+					device.Keypress(roku.SelectKey)
 				}
 			}
 		}
